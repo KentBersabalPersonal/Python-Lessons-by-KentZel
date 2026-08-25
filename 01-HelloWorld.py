8001bb509ae5e721ae31d0f510eefa86bdea71f2
Kent Bersabal <denzelbersabal44@gmail.com>
2026-08-26 07:44:53 +0800

    Hello World In Python

--- 01-HelloWorld.py
+++ 01-HelloWorld.py
@@ -2,3 +2,5 @@ print("\nHello World")
 
 # This is the simplest Python program — just one line.
 # The syntax differs because there is no ";" in python language.
+
+# Notice there's no #include, no main(), and no semicolons — Python runs top to bottom automatically, and doesn't need a starting function.
