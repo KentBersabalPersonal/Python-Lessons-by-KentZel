bdee080084a76b164f586195e1fc6e8e2482d7ae
Kent Bersabal <denzelbersabal44@gmail.com>
2026-08-26 07:49:39 +0800

    Data Types in Python Syntax

--- 02-DataTypes.py
+++ 02-DataTypes.py
@@ -7,3 +7,13 @@ print("\nAge: ", age)
 print("\nGPA: ", gpa)
 print("\nGrade: ", grade)
 print("\nPassing: ", is_passing)
+
+
+# OUTPUT IS:
+# Age: 20
+# GPA: 3.75
+# Grade: A
+# Passing: True
+
+# Variables store data, but unlike C/C++, you don't have to declare a type — Python figures it out automatically based on the value.
+# Unlike C, it does not need any format specifiers like "%d", etc.
