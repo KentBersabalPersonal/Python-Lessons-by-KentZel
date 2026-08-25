eeb41db273494757fcdb6acce2f82da58eb137d3
Kent Bersabal <denzelbersabal44@gmail.com>
2026-08-26 07:53:56 +0800

    Input Strings In Python

---
+++ 03-Input.py
@@ -0,0 +1,8 @@
+name = input("\nEnter your name: ")
+print("\nHello,", name + "!")
+
+# OUTPUT: 
+# Enter your name: (It lets you enter any name you want)
+# Hello, 'The name you Inputted'!
+
+# input() asks the user to type something and returns it directly no need for cin, scanf, or specifying a type beforehand.
