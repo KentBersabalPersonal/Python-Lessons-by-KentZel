f147a5b0951cb668f642eb59b38b8e17e2a9d76f
Kent Bersabal <denzelbersabal44@gmail.com>
2026-08-26 17:29:25 +0800

    For Loops In Python

---
+++ 07-For-Loop.py
@@ -0,0 +1,13 @@
+for i in range(1, 6):
+    print("\nCount:", i)
+
+# OUTPUT IS:
+# Count: 1
+# Count: 2
+# Count: 3
+# Count: 4
+# Count: 5
+
+# MAIN IDEA:
+# Python's for loop works differently from C/C++ — instead of manually setting a counter, condition, and increment, it loops through a
+# sequence of values using range().
