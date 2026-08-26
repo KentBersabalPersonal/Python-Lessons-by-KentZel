9f8c35dc08fc2a1c2b01b1d7eb3e60f512538bd7
Kent Bersabal <denzelbersabal44@gmail.com>
2026-08-26 17:30:37 +0800

    While Loops In Python

---
+++ 08-While-Loop.py
@@ -0,0 +1,14 @@
+i = 1
+while i <= 5:
+    print("\nCount:", i)
+    i += 1
+
+# OUTPUT: 
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# MAIN IDEA: 
+# Repeats code as long as the condition is true — same logic as C/C++.
