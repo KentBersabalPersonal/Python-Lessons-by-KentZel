6f70ecb67ae9e6eebb3909596d91e03be1deca5f
Kent Bersabal <denzelbersabal44@gmail.com>
2026-08-26 08:02:49 +0800

    Type Conversions in Python

---
+++ 04-TypeConversion.py
@@ -0,0 +1,15 @@
+age_text = input("\nEnter your age: ")
+age = int(age_text)   # convert the string into an integer
+print("\nYou are ", age + "years old")
+
+next_year = age + 1
+print("\nNext year you'll be: ", next_year)
+
+# OUTPUT: 
+# Enter your age: (It lets you enter any age)
+# It then convert the String into an Integer using the var = int(var).
+
+# Then it states your next age next year by adding 1 to your age variable.
+# Final Output:
+# You are (Age Inputted) years old
+# Next year you'll be: (Age Inputted + 1)
